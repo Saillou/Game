33a16981267d4d8d55c3b7c27ed401486cf39dfd
Saillou <chauvinremy@upyourprod.fr>
2023-10-31 23:55:38 +0100

    Add filter on folder name for Vs

--- .cmake/config.cmake
+++ .cmake/config.cmake
@@ -1,5 +1,6 @@
-set(TARGET_NAME    "${PROJECT_NAME}")
-set(SOURCE_FOLDER  "${CMAKE_SOURCE_DIR}/Source")
-set(OUTPUT_PATH    "${CMAKE_BINARY_DIR}/Output")
+set(TARGET_NAME     "${PROJECT_NAME}")
+set(SOURCE_FOLDER   "${CMAKE_SOURCE_DIR}/Source")
+set(EXTERNAL_FOLDER "${CMAKE_SOURCE_DIR}/.external")
+set(OUTPUT_PATH     "${CMAKE_BINARY_DIR}/Output")
 
 set(CMAKE_SUPPRESS_REGENERATION true) # Remove "Zero_check" project
