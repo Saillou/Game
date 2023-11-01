a02c04627115409f9fa119b7a79e745e2dd32d2d
Saillou <chauvinremy@upyourprod.fr>
2023-11-01 01:12:48 +0100

    Create empty window

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cassert>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -13,13 +12,36 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
-    auto window = glfwCreateWindow(800, 600, "Game", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Game", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
-    if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-        std::cout << "Success" << std::endl;
-    else
-        std::cout << "Failure" << std::endl;
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
+    // Render
+    glEnable(GL_BLEND);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_PROGRAM_POINT_SIZE);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    do {
+        // Draw
+        glClearColor(0.05f, 0.05f, 0.06f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // ...
+
+        glfwSwapBuffers(window);
+
+        // Inputs
+        glfwPollEvents();
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
+    } while (!glfwWindowShouldClose(window));
+
+    // Clean up
+    glfwTerminate();
     return 0;
 }
