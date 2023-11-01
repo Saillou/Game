9cf37552c3640bcad074205ff2f25a89ce831ffb
Saillou <chauvinremy@upyourprod.fr>
2023-11-01 01:00:28 +0100

    Add GLFW

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <cassert>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    // Init GLFW
+    glfwInit();
+
+    // OpenGL >= 4.0 (for tesselation)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
+    auto window = glfwCreateWindow(800, 600, "Game", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        std::cout << "Success" << std::endl;
+    else
+        std::cout << "Failure" << std::endl;
 
     return 0;
 }
