f8a64bafe660c152002d42409c2cbdc8e96bc8ac
Saillou <chauvinremy@upyourprod.fr>
2023-11-04 21:47:17 +0100

    Remove shaders from compilation

--- .cmake/config.cmake
+++ .cmake/config.cmake
@@ -5,7 +5,6 @@ set(TARGET_NAME     "${PROJECT_NAME}")
 
 set(SOURCE_FOLDER   "${CMAKE_SOURCE_DIR}/Source")
 set(EXTERNAL_FOLDER "${CMAKE_SOURCE_DIR}/.external")
-set(SHADERS_FOLDER  "${CMAKE_SOURCE_DIR}/Shaders")
 set(OUTPUT_FOLDER   "${CMAKE_BINARY_DIR}/Output")
 
 set(CMAKE_SUPPRESS_REGENERATION true) # Remove "Zero_check" project
