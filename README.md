fc86ffe9f084f4e91bf6c17a479e0847b9032045
Saillou <chauvinremy@upyourprod.fr>
2023-11-19 14:31:08 +0100

    Add physic engine

--- README.md
+++ README.md
@@ -19,3 +19,4 @@ Use the command `./generate.ps1` and hope for the best.
 - `Glad` (Get entry points for `opengl32.dll`'s methods) | Generated at: https://glad.dav1d.de
 - `Glfw` (Windows and inputs helper)                              | Official website: https://www.glfw.org
 - `FreeType` (Font renderer)                                             | Clone: https://gitlab.freedesktop.org/freetype
+- `ReactPhysics3D` (Physic Engine)                                     | Clone: https://github.com/DanielChappuis/reactphysics3d
--- Source/Engine/Physx/Physx.cpp
+++ Source/Engine/Physx/Physx.cpp
@@ -28,17 +28,16 @@ void Physx::Clear() {
 	_get().m_static_elements.clear();
 }
 
-#define WORLD_2D
-#ifdef WORLD_2D
-static vec3 to3d(const vec2& v) {
-	return vec3(-v.x, 0.0f, v.y);
-}
-static vec2 to2d(const vec3& v) {
-	return vec2(-v.x, v.z);
-}
-
 void Physx::Compute(float delta_time_ms) {
 	Physx& engine(_get());
+
+	// Helper
+	static const auto to3d = [=](const vec2& v) -> vec3 {
+		return vec3(-v.x, 0.0f, v.y);
+	};
+	static const auto to2d = [=](const vec3& v) -> vec2 {
+		return vec2(-v.x, v.z);
+	};
 
 	// World constantes
 	const float dt_sec = delta_time_ms / 1000.0f;
@@ -157,81 +156,3 @@ void Physx::Compute(float delta_time_ms) {
 		body->position		 = to3d(body_pos);
 	}
 }
-
-#else
-void Physx::Compute(float delta_time_ms) {
-	static bool ignore = false;
-	if (ignore)
-		return;
-
-	Physx& engine(_get());
-
-	// World constantes
-	const float dt_sec = delta_time_ms / 1000.0f;
-	const vec3 GRAVITY(0.0f, 0.0f, -9.81f);
-	
-	// Update positions/velocity
-	for (auto& body : engine.m_dynamic_elements) 
-	{
-		// Bdf
-		vec3 accel = GRAVITY;			    
-		vec3 speed = accel*dt_sec + body->_speedPosition;
-		vec3 position = accel*(dt_sec*dt_sec)/2.0f + body->_speedPosition*dt_sec + body->position;
-
-
-		// Collisions
-		// - Dynamic object variables
-		const auto& Pp0 = body->position;
-		const auto& Vp0 = body->_speedPosition;
-
-		const auto& Pp1 = position;
-		const auto& Vp1 = speed;
-
-		// - Static object variables
-		for (const auto& hindrance : engine.m_static_elements) {
-			if (hindrance->type != BaseBody::ContactType::Facette) {
-				std::cout << "Could not compute collision against a not-facette" << std::endl;
-				continue;
-			}
-
-			const vec3 Pr = hindrance->position;
-			const vec3 Vn = normalize(
-				hindrance->dimensions[2] == vec3(0) ? cross(hindrance->dimensions[1], hindrance->dimensions[0]) : hindrance->dimensions[2]
-			);
-
-			if (body->type != BaseBody::ContactType::Sphere) {
-				std::cout << "Could not compute collision with a not-sphere" << std::endl;
-				continue;
-			}
-
-			// Boum
-			float tc = 0.0f;
-			const auto collide_point = ([=](float& r_time_collide) -> std::optional<vec3>
-				{
-					const float Dp0 = dot(Pp0, Vn);
-					const float Dp1 = dot(Pp1, Vn);
-					if (Dp0 * Dp1 > 0) // no collision
-						return {};
-
-					r_time_collide = std::abs(Dp0) / (std::abs(Dp0) + std::abs(Dp1));
-					return r_time_collide * (Pp1-Pp0) + Pp0;
-				}
-			)(tc);
-			if (!collide_point)
-				continue; // no boum
-
-			ignore = true;
-
-			// Bounce from collide_point during (1-tc)*dt_sec seconds
-			const float dt_c = (1 - tc) * dt_sec;
-
-			speed    = accel*dt_c + body->_speedPosition*(-1.0f); // change sign
-			position = accel*(dt_sec*dt_sec)/2.0f + body->_speedPosition * (-1.0f) *dt_sec + collide_point.value();
-		}
-
-		// Update
-		body->_speedPosition = speed;
-		body->position = position;
-	}
-}
-#endif
